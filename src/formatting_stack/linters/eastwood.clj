79397ba18ee3405c1b107542141e747b61eecb8a
vemv <vemv@users.noreply.github.com>
2021-09-03 21:19:31 +0200

    Upgrade Eastwood

--- src/formatting_stack/linters/eastwood.clj
+++ src/formatting_stack/linters/eastwood.clj
@@ -12,14 +12,8 @@
    (java.io File)))
 
 (def default-eastwood-options
-  ;; Avoid false positives or undesired checks:
-  (let [linters (remove #{:suspicious-test :unused-ret-vals :constant-test :wrong-tag}
-                        eastwood.lint/default-linters)]
-    (-> eastwood.lint/default-opts
-        (assoc :linters linters
-               :rethrow-exceptions? true))))
-
-(def parallelize-linters? (System/getProperty "formatting-stack.eastwood.parallelize-linters"))
+  (-> eastwood.lint/default-opts
+      (assoc :rethrow-exceptions? true)))
 
 (def config-filename "formatting_stack.clj")
 
@@ -32,16 +26,14 @@
                         (remove #(str/ends-with? % ".edn"))
                         (keep ns-name-from-filename))
         reports    (atom nil)
-        exceptions (atom nil)
-        output     (with-out-str
-                     (binding [*warn-on-reflection* true]
-                       (try
-                         (cond-> options
-                           true                 (assoc :namespaces namespaces)
-                           parallelize-linters? (update :builtin-config-files conj config-filename)
-                           true                 (eastwood.lint/eastwood (impl/->TrackingReporter reports)))
-                         (catch Exception e
-                           (swap! exceptions conj e)))))]
+        exceptions (atom nil)]
+    (with-out-str
+      (try
+        (-> options
+            (assoc :namespaces namespaces)
+            (eastwood.lint/eastwood (impl/->TrackingReporter reports)))
+        (catch Exception e
+          (swap! exceptions conj e))))
     (->> @reports
          :warnings
          (map :warn-data)
@@ -53,8 +45,7 @@
                             :filename            (if (string? uri-or-file-name)
                                                    uri-or-file-name
                                                    (-> ^File uri-or-file-name .getCanonicalPath)))))
-         (concat (impl/warnings->reports output)
-                 (impl/exceptions->reports @exceptions)))))
+         (into (impl/exceptions->reports @exceptions)))))
 
 (defn new [{:keys [eastwood-options]
             :or   {eastwood-options {}}}]
